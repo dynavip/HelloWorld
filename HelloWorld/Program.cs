680e44c7059745be0cbb9f6306c7d18e4f59d6b2
plcskoru <plcskoru@PLCL1120>
2023-05-21 17:25:07 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string imie = "Krzysztof";
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My name is Kris");
+        }
+    }
+}
